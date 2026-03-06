6fbfdae232151b2aa67ebdc8925839200a74a0ad
Alcopatriarch <vladuslav2004v87@gmail.com>
2026-03-06 20:48:23 +0100

    feat: implement light and dark themes

--- rs-tandem-app/src/App.tsx
+++ rs-tandem-app/src/App.tsx
@@ -5,15 +5,24 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
+  const [isDark, setDark] = useState(false);
+
+  const toggleTheme = () => {
+    setDark(!isDark);
+  };
+
   return (
-    <>
+    <body className={isDark ? "dark" : ""}>
+      <button onClick={toggleTheme}>Toggle theme</button>
       <div>
         <a href="https://vite.dev" target="_blank"></a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="bg-white text-black dark:bg-black dark:text-white">
+        Vite + React
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,12 +34,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-  	  <div className="min-h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center">
-      	<h1 className="text-5xl md:text-7xl font-black text-white tracking-tight drop-shadow-2xl animate-pulse">
-      	  Tailwind v4 + Vite + React
+      <div className="min-h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center">
+        <h1 className="text-5xl md:text-7xl font-black text-white tracking-tight drop-shadow-2xl animate-pulse">
+          Tailwind v4 + Vite + React
         </h1>
       </div>
-    </>
+    </body>
   );
 }
 
