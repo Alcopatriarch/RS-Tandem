352651f3eb6aea4958d8dd79549ca8de8fee019c
Sergey <lestrwebwork@gmail.com>
2026-03-07 18:52:04 +0300

    feat: add footer component

---
+++ rs-tandem-app/src/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <footer className="absolute top-[906px] left-0 w-[1440px]">
+      <div className="border-t border-[var(--color-american-silver)]"></div>
+      <div className="max-w-[1440px] mx-auto py-[17px] flex justify-center">
+        <span className="text-[32px] text-[var(--color-american-silver)] font-figtree">
+          About us
+        </span>
+      </div>
+    </footer>
+  );
+};
