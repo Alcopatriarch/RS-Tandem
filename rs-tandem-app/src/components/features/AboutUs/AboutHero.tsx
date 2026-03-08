8e53289bc2534b20f2ae0af87f43901e19135972
Alcopatriarch <vladuslav2004v87@gmail.com>
2026-03-08 23:10:27 +0100

    feat: add a page about us

---
+++ rs-tandem-app/src/components/features/AboutUs/AboutHero.tsx
@@ -0,0 +1,22 @@
+import AboutCard from "./AboutCard";
+import { team } from "../../../types/index";
+
+const AboutHero: React.FC = () => {
+  return (
+    <section className="bg-milk dark:bg-dark-gunmetal pb-10">
+      <div className="max-w-6xl mx-auto px-6">
+        <h2 className="text-4xl font-bold text-center mb-16 dark:text-american-silver">
+          Наша Команда
+        </h2>
+
+        <div className="flex flex-wrap justify-center gap-10">
+          {team.map((member) => (
+            <AboutCard key={member.id} member={member} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutHero;
