04f1f5b103ad2ad871c502d82799736f37c96173
Sergey <lestrwebwork@gmail.com>
2026-03-09 05:06:15 +0300

    feat: implement light and dark themes

--- rs-tandem-app/src/components/layout/Header.tsx
+++ rs-tandem-app/src/components/layout/Header.tsx
@@ -3,29 +3,30 @@ import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
+import IconButton from "@mui/material/IconButton";
 
 type HeaderProps = {
   darkMode?: boolean;
   lang?: "RU" | "EN";
   onLangChange?: (newLang: "RU" | "EN") => void;
-  onThemeChange?: (newMode: boolean) => void;
+  onThemeChange: () => void;
 };
 
 export const Header: React.FC<HeaderProps> = ({
   darkMode = true,
   lang = "RU",
   onLangChange,
-  /* onThemeChange,*/
+  onThemeChange,
 }) => {
   return (
     <header
       className={`
         h-14 sm:h-16
-        bg-[var(--color-eerie-black)]
+        bg-[var(--color-eerie-black)]  dark:bg-[var(--color-gainsboro)]
         border-b border-[var(--color-deep-space-sparke)/0.5]
         px-4 sm:px-6
         flex items-center justify-between
-        text-[var(--color-american-silver)]
+        text-[var(--color-american-silver)]  dark:text-[var(--color-dark-gunmetal)]
         font-['Figtree'] weight-medium
         select-none
       `}
@@ -68,48 +69,19 @@ export const Header: React.FC<HeaderProps> = ({
           <MenuItem value="EN">EN</MenuItem>
         </Select>
 
-        <Select
-          value={darkMode ? "dark" : "light"}
-          onChange={(e) => onLangChange?.(e.target.value as "RU" | "EN")}
-          sx={{
-            minWidth: 44,
-            height: 34,
-            padding: "0 8px",
-            color: "var(--color-american-silver)",
-            borderColor: "var(--color-deep-space-sparke)",
-            borderRadius: "6px",
-            backgroundColor: "var(--color-rich-black)",
-            "& .MuiSelect-select": {
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            },
-            "&:hover": {
-              backgroundColor: "var(--color-japanese-indigo)",
-              borderColor: "var(--color-cadet-grey)",
-            },
-            "&.Mui-focused": {
-              borderColor: "var(--color-teal)",
-              boxShadow: "0 0 0 2px rgba(86,124,189,0.25)",
-            },
-            "& .MuiOutlinedInput-notchedOutline": {
-              borderColor: "var(--color-deep-space-sparke)",
-            },
-          }}
-        >
-          <MenuItem value="light">
+        <IconButton onClick={() => onThemeChange()}>
+          {darkMode ? (
             <LightModeIcon
               fontSize="small"
               sx={{ color: "var(--color-cadet-grey)" }}
             />
-          </MenuItem>
-          <MenuItem value="dark">
+          ) : (
             <DarkModeIcon
               fontSize="small"
               sx={{ color: "var(--color-cadet-grey)" }}
             />
-          </MenuItem>
-        </Select>
+          )}
+        </IconButton>
       </div>
     </header>
   );
