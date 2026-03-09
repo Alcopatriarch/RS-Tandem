654093d62070c83e12225223d71da6207b878de0
Sergey <lestrwebwork@gmail.com>
2026-03-09 05:04:45 +0300

    feat: implement light and dark themes

--- rs-tandem-app/src/App.tsx
+++ rs-tandem-app/src/App.tsx
@@ -2,13 +2,22 @@ import { Header } from "./components/layout/Header";
 import { Welcome } from "./pages/Welcome";
 import { Footer } from "./components/layout/Footer";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isDark, setDark] = useState(false);
+
+  const toggleTheme = () => {
+    setDark(!isDark);
+  };
+
   return (
     <div className="absolute top-0 left-0 w-[1440px] h-[986px] bg-[#11212D] font-figtree">
-      <Header darkMode lang="RU" />
-      <Welcome />
-      <Footer />
+      <div className={isDark ? "dark" : ""}>
+        <Header darkMode={isDark} lang="RU" onThemeChange={toggleTheme} />
+        <Welcome darkMode={isDark} />
+        <Footer />
+      </div>
     </div>
   );
 }
